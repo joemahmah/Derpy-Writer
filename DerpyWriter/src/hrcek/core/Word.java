c53eba031e210185951fa0bb68abc8a85f4f16c8
Michael Hrcek <hrcekmj@clarkson.edu>
2015-10-22 20:42:33 -0400

    Added documentation to Word.java

--- DerpyWriter/src/hrcek/core/Word.java
+++ DerpyWriter/src/hrcek/core/Word.java
@@ -31,6 +31,12 @@ import java.util.Map;
 
 /**
  *
+ * This class is the derpy representation of a word. Each word contains information
+ * about the words which follow (up to a global accuracy number). The words following
+ * are stored as strings which means that they can theoretically be used in a
+ * cross-dictionary manner. The global accuracy attribute can cause trouble if
+ * used incorrectly...
+ * 
  * @author Michael Hrcek <hrcekmj@clarkson.edu>
  */
 public class Word implements Serializable {
@@ -43,10 +49,23 @@ public class Word implements Serializable {
     private volatile int rarity;
     private volatile List< Map<String, Integer> > wordsAfter;
     
+    /**
+     * This method is used to set the global accuracy value. Use this method with
+     * care! It can break the program if it is used to change the accuracy beyond
+     * what any words may contain...
+     * 
+     * @param accuracyNumber Higher accuracy == Less derp?
+     */
     public static synchronized void setAccuracyNumber(int accuracyNumber){
         Word.accuracyNumber = accuracyNumber;
     }
     
+    /**
+     * Standard constructor for the word object. This should be used in most
+     * cases.
+     * 
+     * @param name The string representation of the word.
+     */
     public Word(String name) {
         this.name = name;
         this.rarity = 1;
@@ -58,6 +77,13 @@ public class Word implements Serializable {
         
     }
     
+    /**
+     * This is an additional constructor for the word object. It may be used as
+     * a way to create words with custom accuracy.
+     * 
+     * @param name The string representation of the word.
+     * @param size The accuracy of the string.
+     */
     public Word(String name, int size) {
         this.name = name;
         this.rarity = 1;
@@ -69,10 +95,21 @@ public class Word implements Serializable {
         
     }
 
+    /**
+     * This increases the rarity value of the word.
+     * 
+     * @deprecated This is currently unused...
+     */
     public void increaseRarity() {
         rarity++;
     }
 
+    /**
+     * This method adds words to the current word's list of words that follow.
+     * 
+     * @param word Word to be added.
+     * @param index This is how far back the word occurred.
+     */
     public synchronized void addWordAfter(Word word, int index) {
         if (wordsAfter.get(index).containsKey(word)) {
             wordsAfter.get(index).put(word.getName(), wordsAfter.get(index).get(word)+1);
@@ -81,18 +118,43 @@ public class Word implements Serializable {
         }
     }
     
+    /**
+     * This gets a map containing the string representations of the words that
+     * follow this one index words away.
+     * 
+     * @param index This is the number away the occurrence is.
+     * @return A map containing string representations of the words that followed this one.
+     */
     public synchronized Map<String, Integer> getWordsAfter(int index){
         return wordsAfter.get(index);
     }
     
+    /**
+     * This method gets the string representation of the word. This is used when
+     * printing text.
+     * 
+     * @return The string representation of the word.
+     */
     public synchronized String getName() {
         return name;
     }
 
+    /**
+     * This method gets the rarity of this word.
+     * 
+     * @deprecated Not used...
+     * 
+     * @return The rarity of the word.
+     */
     public synchronized int getRarity() {
         return rarity;
     }
     
+    /**
+     * The toString() method... It does what every other toString() method does...
+     * 
+     * @return The string representation of the word.
+     */
     public synchronized String toString(){
         return name;
     }
